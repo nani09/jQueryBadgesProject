07dd210c3b4d6527839352919388e55188e8da27
nani09 <ravishankartati@gmail.com>
2018-01-12 15:47:27 +0530

    Ajax call is added

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,10 @@
+$.ajax({
+  url: 'https://www.codeschool.com/users/2970183.json',
+  dataType: 'jsonp',
+  success: function(response) {
+    // handle response
+  }
+});
 $(function() {
 
   // your code will go here
